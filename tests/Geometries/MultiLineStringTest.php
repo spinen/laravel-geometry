4da88515a6b3f03006120900cfad02a1cb0ad3c1
Jimmy Puckett <jimmy.puckett@spinen.com>
2015-11-10 13:28:43 -0500

    Simple test for the proxy classes

---
+++ tests/Geometries/MultiLineStringTest.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace Spinen\Geometry\Geometries;
+
+use Mockery;
+use Spinen\Geometry\Support\TypeMapper;
+use Spinen\Geometry\TestCase;
+
+class MultiLineStringTest extends TestCase
+{
+    /**
+     * @var MultiLineString
+     */
+    protected $geometry;
+
+    /**
+     * @var Mockery\Mock
+     */
+    protected $geometry_mock;
+
+    /**
+     * @var Mockery\Mock
+     */
+    protected $mapper_mock;
+
+    public function setUp()
+    {
+        parent::setUp();
+
+        $this->setUpMocks();
+
+        $this->geometry = new MultiLineString($this->geometry_mock, $this->mapper_mock);
+    }
+
+    private function setUpMocks()
+    {
+        $this->geometry_mock = Mockery::mock(StdClass::class);
+
+        $this->mapper_mock = Mockery::mock(TypeMapper::class);
+    }
+
+    /**
+     * @test
+     * @group unit
+     */
+    public function it_can_be_constructed()
+    {
+        $this->assertInstanceOf(MultiLineString::class, $this->geometry);
+    }
+}
+
+
---
+++ tests/Geometries/MultiPolygonTest.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace Spinen\Geometry\Geometries;
+
+use Mockery;
+use Spinen\Geometry\Support\TypeMapper;
+use Spinen\Geometry\TestCase;
+
+class MultiPolygonTest extends TestCase
+{
+    /**
+     * @var MultiPolygon
+     */
+    protected $geometry;
+
+    /**
+     * @var Mockery\Mock
+     */
+    protected $geometry_mock;
+
+    /**
+     * @var Mockery\Mock
+     */
+    protected $mapper_mock;
+
+    public function setUp()
+    {
+        parent::setUp();
+
+        $this->setUpMocks();
+
+        $this->geometry = new MultiPolygon($this->geometry_mock, $this->mapper_mock);
+    }
+
+    private function setUpMocks()
+    {
+        $this->geometry_mock = Mockery::mock(StdClass::class);
+
+        $this->mapper_mock = Mockery::mock(TypeMapper::class);
+    }
+
+    /**
+     * @test
+     * @group unit
+     */
+    public function it_can_be_constructed()
+    {
+        $this->assertInstanceOf(MultiPolygon::class, $this->geometry);
+    }
+}
+
+
