f09c0270d01274e6bf18c37ffe8e621114051b2b
Jimmy Puckett <jimmy.puckett@spinen.com>
2016-06-27 13:01:29 -0400

    Declairing the corrd property

--- src/Support/GeometryProxy.php
+++ src/Support/GeometryProxy.php
@@ -24,6 +24,7 @@ use RuntimeException;
  * @method mixed toWkb() Returns the geometry in WKB format.
  * @method mixed toWkt() Returns the geometry in WKT format.
  * @property float acres The acres with in +/-1%
+ * @property array coordinates The points that define the shape
  * @property float square_meters The square meters with in +/-1%
  */
 class GeometryProxy
